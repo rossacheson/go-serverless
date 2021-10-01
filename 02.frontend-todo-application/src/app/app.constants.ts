273fb963cb17ce17b6fc9646c01666cde4cfae3c
Ross Acheson <racheson@fivestardev.com>
2021-10-01 13:58:17 -0400

    more updates of constants

--- 02.frontend-todo-application/src/app/app.constants.ts
+++ 02.frontend-todo-application/src/app/app.constants.ts
@@ -1,10 +1,11 @@
-// export const API_URL = 'http://localhost:7071/api/todos'
-// export const API_URL = 'https://todo-function-app.azurewebsites.net/api/todos'
+// export const API_URL = 'http://localhost:7071/api/todos';
+// export const API_URL = 'https://todo-function-app-rda.azurewebsites.net/api/todos';
+export const API_URL = 'https://todo-function-app-api-mgmt.azure-api.net/todo-function-app-rda/todos';
 
 // export const API_URL = 'https://i5kmidawqk.execute-api.us-east-1.amazonaws.com/dev/todos'; // REST
 // export const API_URL = 'https://thrlrq8kg4.execute-api.us-east-1.amazonaws.com/dev/todos'; // HTTP
 // export const API_URL = 'https://t3qfn134j3.execute-api.us-east-1.amazonaws.com/todos'; // SAM HTTP
-export const API_URL = 'https://upbqmxqpfg.execute-api.us-east-1.amazonaws.com/todos'; // Serverless Framework HTTP
+// export const API_URL = 'https://upbqmxqpfg.execute-api.us-east-1.amazonaws.com/todos'; // Serverless Framework HTTP
 
 export const POOL_DATA = {
     UserPoolId: 'us-east-1_KrPZsAH5u', // 'us-east-1_7dF78dyvw',
